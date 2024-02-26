e654033ab94c06653666f81531d43d6e47827e5c
RiskiManik <riskifirman608@gmail.com>
2024-02-26 14:41:15 +0700

    add fitur authentikasi with email and password

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,16 @@ import { Route, Router } from "@solidjs/router";
 import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
 import "./index.css";
 import { SolidQueryDevtools } from "@tanstack/solid-query-devtools";
+import "toastify-js/src/toastify.css";
 
 import Users from "./pages/Users";
 import Home from "./pages";
 import MangaPage from "./pages/MangaPage";
 import AnimePage from "./pages/AnimePage";
 import Navbar from "./components/Navbar";
+import LoginPage from "./pages/LoginPage";
+import SignUp from "./pages/SignUp";
+import { Provider } from "./context/ContextProvider";
 
 const root = document.getElementById("root");
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
@@ -21,14 +25,18 @@ const queryClient = new QueryClient();
 render(
   () => (
     <QueryClientProvider client={queryClient}>
-      <Navbar />
-      <Router>
-        <Route path="/users" component={Users} />
-        <Route path="/" component={Home} />
-        <Route path="/anime" component={AnimePage} />
-        <Route path="/manga" component={MangaPage} />
-      </Router>
-      <SolidQueryDevtools initialIsOpen={false} />
+      <Provider>
+        <Navbar />
+        <Router>
+          <Route path="/users" component={Users} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/" component={Home} />
+          <Route path="/anime" component={AnimePage} />
+          <Route path="/manga" component={MangaPage} />
+          <SolidQueryDevtools initialIsOpen={false} />
+        </Router>
+      </Provider>
     </QueryClientProvider>
   ),
   root!
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,26 @@
-import type { Component } from "solid-js";
+import { createEffect, createSignal, onMount, type Component } from "solid-js";
+import { AuthSession } from "@supabase/supabase-js";
+import { useNavigate } from "@solidjs/router";
+import { supabase } from "../lib/supabaseClient";
+import getSession from "../features/auth/services/getSession";
 
 const Home: Component = () => {
+  const [session, setSession] = createSignal<AuthSession | null>(null);
+  const navigate = useNavigate();
+
+  onMount(async () => {
+    const res = await getSession();
+    setSession(res.data.session);
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    if (!session()?.access_token) {
+      navigate("/login", { replace: true });
+    }
+  });
+
   return (
     <div class="hero min-h-screen bg-base-200">
       <div class="hero-content flex-col lg:flex-row">
@@ -9,7 +29,7 @@ const Home: Component = () => {
           class="max-w-sm rounded-lg shadow-2xl"
         />
         <div>
-          <h1 class="text-5xl font-bold">Box Office News!</h1>
+          <h1 class="text-5xl font-bold">{session()?.user.email}</h1>
           <p class="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
